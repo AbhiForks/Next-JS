a2cf38b79c62864059683c65c6315dfef4454f13
Abhilash <abhilashvishwa12@gmail.com>
2025-04-05 12:31:43 +0530

    learned NEXT JS Routing

---
+++ 2-nestedroute/src/app/products/allproducts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const allProducts = () => {
+  return (
+    <div>1220 products in the store</div>
+  )
+}
+
+export default allProducts
---
+++ 2-nestedroute/src/app/products/specificproduct/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const specificProduct = () => {
+  return (
+    <div>Here's the one and only product</div>
+  )
+}
+
+export default specificProduct
